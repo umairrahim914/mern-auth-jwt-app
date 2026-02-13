89baac21b4ee767ba84e00645f95ee6b76d8115e
Muhammad Umair Shaukat <uk742609@gmail.com>
2026-02-13 19:07:27 +0500

    Initial backend setup with authentication APIs and JWT
    - Setup Express server
    - Connected MongoDB
    - Created User model
    - Implemented register and login routes
    - Added JWT authentication
    - Configured environment variables

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+
+const router = express.Router();
+
+// Register
+router.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const existingUser = await User.findOne({ email });
+  if (existingUser) return res.status(400).json({ message: "User exists" });
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword
+  });
+
+  res.json({ message: "User registered successfully" });
+});
+
+// Login
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) return res.status(400).json({ message: "Invalid credentials" });
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
+
+  const token = jwt.sign(
+    { id: user._id, role: user.role },
+    process.env.JWT_SECRET,
+    { expiresIn: "1d" }
+  );
+
+  res.json({ token });
+});
+
+module.exports = router;
