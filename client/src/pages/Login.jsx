7e066cb0be4441e9ac1b4396e2c170570240b149
Muhammad Umair Shaukat <uk742609@gmail.com>
2026-02-14 18:22:09 +0500

    Phase 3: Connected frontend login with backend API using axios

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,15 +1,44 @@
+import { useState } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        "http://localhost:5000/api/auth/login",
+        { email, password }
+      );
+
+      alert("Login Successful");
+      console.log(res.data);
+    } catch (error) {
+      alert("Login Failed");
+      console.log(error.response.data);
+    }
+  };
+
   return (
     <div className="container">
       <div className="form-box">
         <h2>Login</h2>
 
-        <input type="email" placeholder="Email Address" onChange={(e) => setEmail(e.target.value)}/>
-        <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
+        <input
+          type="email"
+          placeholder="Enter Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
-        <button>Login</button>
+        <input
+          type="password"
+          placeholder="Enter Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button onClick={handleLogin}>Login</button>
 
         <div className="link-text">
           Don't have an account? <Link to="/register">Register</Link>
